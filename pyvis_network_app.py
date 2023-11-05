74b6fa68cf4ee7f42700ebb4e10df0e8d132de82
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-11-05 10:14:30 +0100

    -push

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -45,7 +45,29 @@ else:
 
     # We then use the NetworkX library to read the edgelist as a directed graph (DiGraph) and define it G.
     G = nx.read_edgelist(edgelist_file, create_using=nx.DiGraph(), data=True)
+    # We also load the json file containing an inList, inWeight, outList, outWeight, and usernameList.
+    data = pd.read_json("https://raw.githubusercontent.com/CamillaSSvendsen/M2/main/congress_network_data.json")
+    # From this file we are interested in the list of usernames.
+    # In this step, we iterate through the edges of the graph G and add data to the DataFrame.
+    # The for loop extracts the source node, the target node, and the weight for each edge in the graph, and assigns these values in the DataFrame.
+    for a, b, wei in G.edges(data=True):
+        source, target, weight = a, b, wei.get('weight', None)
+        df = df.append({'Source': source, 'Target': target, 'Weight': weight}, ignore_index=True)
+    
+    # We create a new empty dataframe only consisting of column names "id" and "name"
+    names_df = pd.DataFrame(columns=['node_id', 'name'])
 
+    # We want to have a way to connect the usernames from the list in the json file to the nodes in the edgelist/dataframe.
+    # We apply an index to the usernameList in the json file "data", and combine the node number in the graph to a username from the json file "data".
+    i = 0
+    for node in G.nodes():
+        G.add_node(node, name=data['usernameList'][0][i])
+        i += 1
+    
+    # We set up a networkx layout, to make sure, we get the same layout, evertime we load a visualization of a network.
+    G_layout = nx.layout.kamada_kawai_layout(G)
+
+    
 
     # Initiate PyVis network object
     drug_net = Network(
