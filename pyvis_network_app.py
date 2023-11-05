c086a5b017ced72391e32a55a0a59b1c244f7c12
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-11-05 10:05:34 +0100

    -push

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -13,46 +13,81 @@ hv.extension('bokeh')
 from bokeh.plotting import show
 
 
-# We define the URL for the raw edgelist file from our created GitHub repository..
-github_edgelist_url = 'https://raw.githubusercontent.com/CamillaSSvendsen/M2/main/congress.edgelist'
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
 
-# To get access to the data in the edgelist file from the GitHub repository, we use the urllib.request library.
-edgelist_file = urllib.request.urlopen(github_edgelist_url)
-
-# We then use the NetworkX library to read the edgelist as a directed graph (DiGraph) and define it G.
-G = nx.read_edgelist(edgelist_file, create_using=nx.DiGraph(), data=True)
-# The default is undirected graph.
-# data=True is necessary since the edgelist file contains additional information (data) associated with the edges. The additional information is weight.
-
-
-
-# Read dataset (CSV)
 
 # Set header title
 st.title('Network Graph Visualization of Drug-Drug Interactions')
 
-# Initiate PyVis network object
-drug_net = Network()
+# Define list of selection options and sort alphabetically
+drug_list = ['Metformin', 'Glipizide', 'Lisinopril', 'Simvastatin',
+            'Warfarin', 'Aspirin', 'Losartan', 'Ibuprofen']
+drug_list.sort()
+
+# Implement multiselect dropdown menu for option selection (returns a list)
+selected_drugs = st.multiselect('Select drug(s) to visualize', drug_list)
+
+# Set info message on initial site load
+if len(selected_drugs) == 0:
+    st.text('Choose at least 1 drug to start')
+
+# Create network graph when user selects >= 1 item
+else:
+    # Create networkx graph object from pandas dataframe
+    github_edgelist_url = 'https://raw.githubusercontent.com/CamillaSSvendsen/M2/main/congress.edgelist'
+
+    # To get access to the data in the edgelist file from the GitHub repository, we use the urllib.request library.
+    edgelist_file = urllib.request.urlopen(github_edgelist_url)
+
+    # We then use the NetworkX library to read the edgelist as a directed graph (DiGraph) and define it G.
+    G = nx.read_edgelist(edgelist_file, create_using=nx.DiGraph(), data=True)
 
 
-# Take Networkx graph and translate it to a PyVis graph format
-drug_net.from_nx(G)
+    # Initiate PyVis network object
+    drug_net = Network(
+                       height='400px',
+                       width='100%',
+                       bgcolor='#222222',
+                       font_color='white'
+                      )
 
-# Generate network with specific layout settings
-drug_net.repulsion(node_distance=420)
-# Save and read graph as HTML file (on Streamlit Sharing)
+    # Take Networkx graph and translate it to a PyVis graph format
+    drug_net.from_nx(G)
 
-try:
-    path = '/tmp'
-    drug_net.save_graph(f'{path}/pyvis_graph.html')
-    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+    # Generate network with specific layout settings
+    drug_net.repulsion(
+                        node_distance=420,
+                        central_gravity=0.33,
+                        spring_length=110,
+                        spring_strength=0.10,
+                        damping=0.95
+                       )
+
+    # Save and read graph as HTML file (on Streamlit Sharing)
+    try:
+        path = '/tmp'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Save and read graph as HTML file (locally)
-except:
-    path = '/html_files'
-    drug_net.save_graph(f'{path}/pyvis_graph.html')
-    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+    except:
+        path = '/html_files'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=435)
 
-# Load HTML file in HTML component for display on Streamlit page
-components.html(HtmlFile.read(), height=435)
+# Footer
+st.markdown(
+    """
+    <br>
+    <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">GitHub Repo</a></h6>
+    <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
+    <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
+    """, unsafe_allow_html=True
+    )
