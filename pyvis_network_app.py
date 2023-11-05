f2f8d27c00233b10c7b748aea520a6ae8d2381aa
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-11-05 10:22:26 +0100

    -push

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -37,7 +37,6 @@ if len(selected_drugs) == 0:
 
 # Create network graph when user selects >= 1 item
 else:
-    # Create networkx graph object from pandas dataframe
     github_edgelist_url = 'https://raw.githubusercontent.com/CamillaSSvendsen/M2/main/congress.edgelist'
 
     # To get access to the data in the edgelist file from the GitHub repository, we use the urllib.request library.
@@ -45,16 +44,18 @@ else:
 
     # We then use the NetworkX library to read the edgelist as a directed graph (DiGraph) and define it G.
     G = nx.read_edgelist(edgelist_file, create_using=nx.DiGraph(), data=True)
+
     # We also load the json file containing an inList, inWeight, outList, outWeight, and usernameList.
     data = pd.read_json("https://raw.githubusercontent.com/CamillaSSvendsen/M2/main/congress_network_data.json")
-    # From this file we are interested in the list of usernames.
+
+    # From this file, we are interested in the list of usernames.
     # In this step, we iterate through the edges of the graph G and add data to the DataFrame.
-    # The for loop extracts the source node, the target node, and the weight for each edge in the graph, and assigns these values in the DataFrame.
+    # The for loop extracts the source node, the target node, and the weight for each edge in the graph and assigns these values in the DataFrame.
     df = pd.DataFrame(columns=['Source', 'Target', 'Weight'])
     for a, b, wei in G.edges(data=True):
         source, target, weight = a, b, wei.get('weight', None)
-        df = df.append({'Source': source, 'Target': target, 'Weight': weight}, ignore_index=True)
-    
+        df = df._append({'Source': source, 'Target': target, 'Weight': weight}, ignore_index=True)
+
     # We create a new empty dataframe only consisting of column names "id" and "name"
     names_df = pd.DataFrame(columns=['node_id', 'name'])
 
