3dca009f15f331cce0feb51be044f8a16f4b18fb
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-11-05 11:01:31 +0100

    -push

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -47,10 +47,16 @@ else:
                        )
 
     # Save and read graph as HTML file (on Streamlit Sharing)
-    
-    path = '/Users/patrickprintz/Desktop/Universitet/Pyvis'
-    drug_net.save_graph(f'{path}/pyvis_graph.html')
-    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+    try:
+        path = '/tmp'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/html_files'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Load HTML file in HTML component for display on Streamlit page
     components.html(HtmlFile.read(), height=435)
