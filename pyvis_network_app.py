7dabcb069b0850dd139c945f6c3d3684f675e6b5
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-11-05 10:58:09 +0100

    -push

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -3,42 +3,70 @@ import streamlit.components.v1 as components
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
-import pyvis
-import json
-import urllib.request # This library is used to get access to the edgelist file in the GitHub repository
-from pyvis.network import Network
-import holoviews as hv
-from holoviews import opts
-hv.extension('bokeh')
-from bokeh.plotting import show
 
-
-import streamlit as st
-import streamlit.components.v1 as components
-import pandas as pd
-import networkx as nx
-from pyvis.network import Network
-import networkx as nx
-import networkx as nx
-from pyvis.network import Network
-
-
+# Read dataset (CSV)
 # Set header title
-st.title('Network Graph Visualization of Drug-Drug hej Interactions')
+st.title('Network Graph Visualization of Drug-Drug Interactions')
 
+# Define list of selection options and sort alphabetically
+drug_list = ['Metformin', 'Glipizide', 'Lisinopril', 'Simvastatin',
+            'Warfarin', 'Aspirin', 'Losartan', 'Ibuprofen']
+drug_list.sort()
 
+# Implement multiselect dropdown menu for option selection (returns a list)
+selected_drugs = st.multiselect('Select drug(s) to visualize', drug_list)
 
-nx_graph = nx.cycle_graph(10)
-nx_graph.nodes[1]['title'] = 'Number 1'
-nx_graph.nodes[1]['group'] = 1
-nx_graph.nodes[3]['title'] = 'I belong to a different group!'
-nx_graph.nodes[3]['group'] = 10
-nx_graph.add_node(20, size=20, title='couple', group=2)
-nx_graph.add_node(21, size=15, title='couple', group=2)
-nx_graph.add_edge(20, 21, weight=5)
-nx_graph.add_node(25, size=25, label='lonely', title='lonely node', group=3)
+# Set info message on initial site load
+if len(selected_drugs) == 0:
+    st.text('Choose at least 1 drug to start')
 
-nt = Network('500px', '500px')
-nt.from_nx(nx_graph)
-nt.show('nx.html')
+# Create network graph when user selects >= 1 item
+else:
+    G = nx.cycle_graph(10)
 
+    # Create networkx graph object from pandas dataframe
+
+    # Initiate PyVis network object
+    drug_net = Network(
+                       height='400px',
+                       width='100%',
+                       bgcolor='#222222',
+                       font_color='white'
+                      )
+
+    # Take Networkx graph and translate it to a PyVis graph format
+    drug_net.from_nx(G)
+
+    # Generate network with specific layout settings
+    drug_net.repulsion(
+                        node_distance=420,
+                        central_gravity=0.33,
+                        spring_length=110,
+                        spring_strength=0.10,
+                        damping=0.95
+                       )
+
+    # Save and read graph as HTML file (on Streamlit Sharing)
+    try:
+        path = '/Users/patrickprintz/Desktop/Universitet/Pyvis/'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/Users/patrickprintz/Desktop/Universitet/Pyvis/'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=435)
+
+# Footer
+st.markdown(
+    """
+    <br>
+    <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">GitHub Repo</a></h6>
+    <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
+    <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
+    """, unsafe_allow_html=True
+    )
